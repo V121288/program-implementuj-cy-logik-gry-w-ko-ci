5397e4bb419778653d8d47cb533e2602eb8e9f62
V121288 <kacperwilda@proton.me>
2024-10-16 19:08:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ile kostek chcesz rzucić?(3 - 10)");
+        int wybrane = int.Parse(Console.ReadLine());
+        int[] tablica = new int[wybrane];
+        while (wybrane < 3 || wybrane > 10) 
+        {
+            Console.WriteLine("Ile kostek chcesz rzucić?(3 - 10)");
+            wybrane = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("-----------------");
+        Random random = new Random();
+        int c = 0;
+        int suma = 0;
+        for (int i = 0; i < wybrane; i++) 
+        {
+            c++;
+           int randomowa = random.Next(1, 7);
+            tablica[i] = randomowa;
+            Console.WriteLine($" Kostka {c}: {tablica[i]}");
+            suma = suma + tablica[i];
+        }
+        Console.WriteLine($"Suma wyrzuconych oczek to: {suma}");
+
+
+    }
+}
